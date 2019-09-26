5d02d3c3c21150849be8bf0da0249f413d1cf272
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-09-26 15:11:00 +0200

    Update db_manager.php
    aggiunto gestione notifiche

--- applicazione/models/db_manager.php
+++ applicazione/models/db_manager.php
@@ -1,20 +1,34 @@
 <?php
 
-require_once ('database.php');
+require_once('database.php');
 
-class db_manager{
+class db_manager
+{
 
-    public static function executeQuery($query){
+    public static function executeQuery($query)
+    {
         $conn = database::getConnection();
         $results = array();
         $result = $conn->query($query);
-        if ($result->num_rows > 0) {
-            while($row = $result->fetch_assoc()) {
-                array_push($results, $row);
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    array_push($results, $row);
+                }
+            } else {
+                return NULL;
             }
-        }else{
-            return NULL;
-        }
         return $results;
     }
+
+    public static function execute($query){
+        $conn = database::getConnection();
+        $conn->query($query);
+    }
+
+    public static function getAlerts(){
+        $results = db_manager::executeQuery("SELECT * FROM users where pass ='".$_COOKIE['backup_site']."'");
+        $alerts = db_manager::executeQuery("SELECT * FROM alerts where level <=".$results[0]['access']);
+        return $alerts;
+    }
+
 }
