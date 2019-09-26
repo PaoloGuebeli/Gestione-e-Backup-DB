be77ab46ad5bdffc7d25c882b243d88fdc66d79f
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-09-26 15:10:04 +0200

    Update login.php
    aggiunto controlli sul login
    e il logout

--- applicazione/controllers/login.php
+++ applicazione/controllers/login.php
@@ -7,7 +7,8 @@ class login
 
     public function index()
     {
-        if(isset($_COOKIE['backup_site'])){
+        if($this->check()){
+            $alerts = db_manager::getAlerts();
             require 'views/home.php';
         }else{
             require 'views/login.php';
@@ -33,31 +34,45 @@ class login
     }
 
     public function admin(){
-        $results = db_manager::executeQuery("SELECT * FROM users where pass ='".$_COOKIE['backup_site']."'");
-        if($results !== NULL) {
-            if ($results[0]['access'] > 1) {
-                return true;
+        if(isset($_COOKIE['backup_site'])) {
+            $results = db_manager::executeQuery("SELECT * FROM users where pass ='" . $_COOKIE['backup_site'] . "'");
+            if ($results !== NULL) {
+                if ($results[0]['access'] > 1) {
+                    return true;
+                } else {
+                    return false;
+                }
             } else {
                 return false;
             }
-        }else{
-            return false;
         }
     }
 
     public function verify()
     {
         if($_POST['email'] != null){
-            $results = db_manager::executeQuery("SELECT pass from users where email ='".$_POST['email']."'");
+            $results = db_manager::executeQuery("SELECT * from users where email ='".$_POST['email']."'");
             $hash = $results[0]['pass'];
             if (password_verify($_POST['pass'], $hash)) {
                 setcookie('backup_site', $hash, time() + (86400 * 30), "/");
-                sleep(3);
-                require 'views/home.php';
+                if($results[0]['access'] > 0) {
+                    $first = true;
+                    require 'views/home.php';
+                }else{
+
+                    $_POST['error'] = 'account non verificato';
+                    $this->index();
+                }
             } else {
                 $_POST['error'] = 'username o password non validi';
                 $this->index();
             }
         }
+    }
+
+    public function logout(){
+        unset($_COOKIE['backup_site']);
+        setcookie('backup_site', null, -1, '/');
+        $this->index();
     }
 }
