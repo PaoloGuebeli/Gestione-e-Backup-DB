48a01aeeb8e39e9a340438f63e12aabb7299ba55
PaoloGuebeli <37575527+PaoloGuebeli@users.noreply.github.com>
2019-10-08 16:07:26 +0200

    Aggiunto funzionalità backup
    Prendere i dati per le tabelle dal database

--- applicazione/controllers/backup.php
+++ applicazione/controllers/backup.php
@@ -1,12 +1,33 @@
 <?php
 
 require_once ('models/db_manager.php');
-require_once ('login.php');
+require_once ('models/validate.php');
+/**
+ * Controllo se l'utente può accedere
+ */
+if(validate::check()) {
 
-class backup{
+    class backup
+    {
 
-    public function home(){
-        $alerts = db_manager::getAlerts();
-        require('views/backup.php');
+        public function home()
+        {
+        	$databases = $this->getDatabases();
+        	$settings = $this->getSettings();
+            $alerts = db_manager::getAlerts();
+            require('views/backup.php');
+        }
+
+		public function startBackup($id){
+
+		}
+
+		private function getDatabases(){
+        	return db_manager::executeQuery('SELECT name, settings_id from database_data');
+		}
+
+		private function getSettings(){
+			return db_manager::executeQuery('SELECT * from settings');
+		}
     }
 }
